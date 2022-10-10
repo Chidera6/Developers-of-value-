28f763ca7be533b44aed5b314c9ba556e2c77904
chidera6 <dictasteil@gmail.com>
2022-10-09 22:54:34 -0700

    turned seating capacity to a function

--- Inheritance/oop1.py
+++ Inheritance/oop1.py
@@ -1,14 +1,17 @@
 class Vehicle:
     color = 'white'
-    seating_capacity = 50
 
     def __init__(self,max_speed,mileage):
         self.max_speed = max_speed
         self.mileage = mileage
 
     def fare(self):
-        fare_charge = Vehicle.seating_capacity * 100
+        fare_charge = self.capacity * 100
         return fare_charge
+
+    def seating_capacity(self,capacity=50):
+        self.capacity = capacity
+        return self.capacity
 
     def __str__(self):
         return f"Cars maximum speed is {self.max_speed} and mileage is {self.mileage} and color is {Vehicle.color}"
