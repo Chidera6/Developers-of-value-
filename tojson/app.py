d658638737fc7fa9c4636a1d873507da5aed1f3f
chidera6 <dictasteil@gmail.com>
2022-10-31 03:46:27 -0700

    reading files

--- tojson/app.py
+++ tojson/app.py
@@ -3,8 +3,16 @@ def fileread(filename):
     user = open(filename, "r")
     final_user = user.read()
     print(final_user)
-
-
+    convertjson = json.loads(final_user)
+    x = {
+        "name":"Bless Okafor",
+        "email":"xyz@bob.com",
+        "age":20,
+        "profession":" UI/UX designer"
+    }
+    convertjson["bles665"] = x
+    print(convertjson)
+    
 
 
 x = fileread("info.json")
